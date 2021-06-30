f41c9705afa35f4387a2d6ac5dead0ea3a59789f
Pierre Talbot <pierre.talbot@uni.lu>
2021-06-30 13:22:40 +0200

    Add the lattice of decreasing integers ZDec. Remove Dual because it is not very clear yet how to generically obtain the dual of an abstract domain.

--- include/z.hpp
+++ include/z.hpp
@@ -21,7 +21,7 @@ private:
 
   CUDA ZInc() {}
 public:
-  typedef ZInc LogicalElement;
+  typedef this_type LogicalElement;
 
   /** Similar to \f$[\![\mathit{true}]\!]\f$. */
   CUDA static ZInc bot() {
@@ -57,7 +57,6 @@ public:
     - If `appx` is EXACT: `op` can be >= or >.
     - If `appx` is UNDER: `op` can be >=, > or !=.
     - If `appx` is OVER: `op` can be >=, > or ==.
-  An empty optional is also returned if the element would overflow in `int_type`.
     */
   template<typename Formula>
   CUDA thrust::optional<LogicalElement> interpret(Approx appx, const Formula& f) {
@@ -149,12 +148,12 @@ public:
     return make_x_op_i(Formula<Allocator>::GEQ, 0, value, allocator);
   }
 
-  /** Print the current element with the logical name of the variables. */
+  /** Print the current element. */
   CUDA void print() const {
-    if(value == bot().value) {
+    if(is_bot()) {
       printf("%c", 0x22A5);
     }
-    else if(value == top().value) {
+    else if(is_top()) {
       printf("%c", 0x22A4);
     }
     else if(value >= 0) {
@@ -162,6 +161,164 @@ public:
     }
     else {
       printf("%lld", (long long int) value);
+    }
+  }
+
+  template <typename VT2, typename Alloc2>
+  friend class ZDec;
+};
+
+/** The lattice of decreasing integers. */
+template<typename VT, typename Alloc>
+class ZDec {
+public:
+  /** The dual lattice of ZDec.
+   * Note, however, that the interpretation function is not dually equivalent.
+   * This is still not very clear what is the dual of an abstract domain as a whole. */
+  typedef ZInc<VT, Alloc> DualType;
+  typedef typename DualType::ValueType ValueType;
+  typedef typename DualType::Allocator Allocator;
+  typedef ZDec<ValueType, Allocator> this_type;
+private:
+  DualType dual;
+
+  CUDA ZDec() {}
+  CUDA ZDec(DualType dual): dual(dual) {}
+public:
+  typedef this_type LogicalElement;
+
+  /** Similar to \f$[\![\mathit{true}]\!]\f$. */
+  CUDA static ZDec bot() {
+    return ZDec(DualType::top());
+  }
+
+  /** Similar to \f$[\![\mathit{false}]\!]\f$. */
+  CUDA static ZDec top() {
+    return ZDec(DualType::bot());
+  }
+
+  CUDA explicit operator ValueType() const { return static_cast<ValueType>(dual); }
+
+  /** Similar to \f$[\![x \geq i]\!]\f$ for any name `x`. */
+  template<typename VT2>
+  CUDA ZDec(VT2 i): dual(i) {}
+
+  template<typename Allocator2>
+  CUDA ZDec(const ZDec<ValueType, Allocator2>& i): dual(i.dual) {}
+  CUDA ZDec(const this_type& i): dual(i.dual) {}
+
+  CUDA bool operator==(const this_type& other) const {
+    return dual == other.dual;
+  }
+
+  /** Expects a predicate of the form `x <op> i` where `x` is any variable's name, and `i` an integer.
+    - If `appx` is EXACT: `op` can be <= or <.
+    - If `appx` is UNDER: `op` can be <=, < or !=.
+    - If `appx` is OVER: `op` can be <=, < or ==.
+    */
+  template<typename Formula>
+  CUDA thrust::optional<LogicalElement> interpret(Approx appx, const Formula& f) {
+    typedef Formula F;
+    if(f.tag == F::TRUE) {
+      return bot();
+    }
+    else if(f.tag == F::FALSE) {
+      return top();
+    }
+    if(SHAPE(f, F::LEQ, F::AVAR, F::LONG)) {      // x <= 4
+      return ZDec(f.children[1].i);
+    }
+    else if(SHAPE(f, F::LT, F::AVAR, F::LONG)) {  // x < 4
+      return ZDec(f.children[1].i - 1);
+    }
+    // Under-approximation of `x != 4` as `3`.
+    else if(SHAPE(f, F::NEQ, F::AVAR, F::LONG) && appx == UNDER) {
+      return ZDec(f.children[1].i - 1);
+    }
+    // Over-approximation of `x == 4` as `4`.
+    else if(SHAPE(f, F::EQ, F::AVAR, F::LONG) && appx == OVER) {
+      return ZDec(f.children[1].i);
+    }
+    return {};
+  }
+
+  /** `true` whenever \f$ a = \top \f$, `false` otherwise. */
+  CUDA bool is_top() const {
+    return dual.is_bot();
+  }
+
+  /** `true` whenever \f$ a = \bot \f$, `false` otherwise. */
+  CUDA bool is_bot() const {
+    return dual.is_top();
+  }
+
+  /** \f$ a \sqcup b = \mathit{min}(a,b) \f$. */
+  CUDA this_type& join(const LogicalElement& other) {
+    dual.meet(other.dual);
+    return *this;
+  }
+
+  /** \f$ a \sqcap b = \mathit{max}(a,b) \f$. */
+  CUDA this_type& meet(const LogicalElement& other) {
+    dual.join(other.dual);
+    return *this;
+  }
+
+  /** Has no effect.
+     \return always `false`. */
+  CUDA bool refine() { return false; }
+
+  /** \f$ a \models \varphi \f$ is defined as \f$ a \leq [\![\varphi]\!] \f$. */
+  CUDA bool entailment(const LogicalElement& other) const {
+    return ValueType(dual) <= ValueType(other);
+  }
+
+  template<typename Allocator = Alloc>
+  CUDA DArray<LogicalElement, Allocator> split(const Allocator& allocator = Allocator()) const {
+    if(is_top()) {
+      return DArray<LogicalElement, Allocator>();
+    }
+    else {
+      return DArray<LogicalElement, Allocator>(1, *this, allocator);
+    }
+  }
+
+  /** Reset the internal counter to the one of `other`. */
+  CUDA void reset(const this_type& other) {
+    dual.reset(other.dual);
+  }
+
+  /** \return A copy of the current abstract element. */
+  CUDA this_type clone() const {
+    return *this;
+  }
+
+  /** \return \f$ _ \leq i \f$ where `_` is an arbitrary variable's name and `i` the integer value.
+  `true` is returned whenever \f$ a = \bot \f$ and `false` whenever \f$ a = \top \f$. */
+  template<typename Allocator = Alloc>
+  CUDA Formula<Allocator> deinterpret(const Allocator& allocator = Allocator()) const {
+    if(is_top()) {
+      return Formula<Allocator>::make_false();
+    }
+    else if(is_bot()) {
+      return Formula<Allocator>::make_true();
+    }
+    return make_x_op_i(Formula<Allocator>::LEQ, 0, ValueType(dual), allocator);
+  }
+
+  /** Print the current element. */
+  CUDA void print() const {
+    if(is_bot()) {
+      printf("%c", 0x22A5);
+    }
+    else if(is_top()) {
+      printf("%c", 0x22A4);
+    }
+    else if(ValueType(dual) >= 0) {
+      printf("%llu", (unsigned long long int) ValueType(dual));
+    }
+    else {
+      printf("%lld", (long long int) ValueType(dual));
     }
   }
 };
--- tests/z_test.cpp
+++ tests/z_test.cpp
@@ -11,61 +11,80 @@
 using namespace lala;
 
 typedef ZInc<int, StandardAllocator> zi;
+typedef ZDec<int, StandardAllocator> zd;
 typedef Formula<StandardAllocator> F;
-
-void test_formula(Approx appx, const F& f, thrust::optional<zi> expect) {
-  thrust::optional<zi> j = zi::bot().interpret(appx, f);
-  EXPECT_EQ(j.has_value(), expect.has_value());
-  EXPECT_EQ(j, expect);
-}
 
 TEST(ZDeathTest, BadConstruction) {
   ASSERT_DEATH(zi(Limits<int>::bot()), "");
   ASSERT_DEATH(zi(Limits<int>::top()), "");
+  // Dual
+  ASSERT_DEATH(zd(Limits<int>::bot()), "");
+  ASSERT_DEATH(zd(Limits<int>::top()), "");
 }
 
-TEST(ZTest, ValidInterpret) {
-  test_formula(
-    EXACT,
-    make_x_op_i(F::GEQ, 0, 10, standard_allocator),
-    zi(10));
-  test_formula(
-    EXACT,
-    make_x_op_i(F::GT, 0, 10, standard_allocator),
-    zi(11));
-  test_formula(
-    UNDER,
-    make_x_op_i(F::NEQ, 0, 10, standard_allocator),
-    zi(11));
-  test_formula(
-    OVER,
-    make_x_op_i(F::EQ, 0, 10, standard_allocator),
-    zi(10));
+template<typename VarDom>
+void test_formula(Approx appx, const F& f, thrust::optional<VarDom> expect) {
+  thrust::optional<VarDom> j = VarDom::bot().interpret(appx, f);
+  EXPECT_EQ(j.has_value(), expect.has_value());
+  EXPECT_EQ(j, expect);
 }
 
-TEST(ZTest, NoInterpret) {
-  test_formula(
-    EXACT,
-    make_x_op_i(F::NEQ, 0, 10, standard_allocator),
-    {});
-  test_formula(
-    EXACT,
-    make_x_op_i(F::EQ, 0, 10, standard_allocator),
-    {});
+template<typename VarDom>
+void test_interpret(F::Type relation, Approx appx, typename VarDom::ValueType elem, thrust::optional<VarDom> expect) {
+  test_formula<VarDom>(
+    appx,
+    make_x_op_i(relation, 0, elem, standard_allocator),
+    expect);
+}
+
+template<typename VarDom>
+void test_all_interpret(F::Type relation, typename VarDom::ValueType elem, thrust::optional<VarDom> expect) {
   Approx appxs[3] = {EXACT, UNDER, OVER};
   for(int i = 0; i < 3; ++i) {
-    test_formula(
-      appxs[i],
-      make_x_op_i(F::LEQ, 0, 10, standard_allocator),
-      {});
-    test_formula(
-      appxs[i],
-      make_x_op_i(F::LT, 0, 10, standard_allocator),
-      {});
+    test_interpret<VarDom>(relation, appxs[i], elem, expect);
   }
 }
 
-// `a` and `b` are supposed ordered and `a < b`.
+template<typename VarDom>
+void test_exact_interpret(F::Type relation, typename VarDom::ValueType elem, thrust::optional<VarDom> expect) {
+  test_interpret<VarDom>(relation, EXACT, elem, expect);
+}
+
+template<typename VarDom>
+void test_under_interpret(F::Type relation, typename VarDom::ValueType elem, thrust::optional<VarDom> expect) {
+  test_interpret<VarDom>(relation, UNDER, elem, expect);
+}
+
+template<typename VarDom>
+void test_over_interpret(F::Type relation, typename VarDom::ValueType elem, thrust::optional<VarDom> expect) {
+  test_interpret<VarDom>(relation, OVER, elem, expect);
+}
+
+TEST(ZTest, ValidInterpret) {
+  test_all_interpret<zi>(F::GEQ, 10, zi(10));
+  test_all_interpret<zi>(F::GT, 10, zi(11));
+  test_under_interpret<zi>(F::NEQ, 10, zi(11));
+  test_over_interpret<zi>(F::EQ, 10, zi(10));
+  // Dual
+  test_all_interpret<zd>(F::LEQ, 10, zd(10));
+  test_all_interpret<zd>(F::LT, 10, zd(9));
+  test_under_interpret<zd>(F::NEQ, 10, zd(9));
+  test_over_interpret<zd>(F::EQ, 10, zd(10));
+}
+
+TEST(ZTest, NoInterpret) {
+  test_exact_interpret<zi>(F::NEQ, 10, {});
+  test_exact_interpret<zi>(F::EQ, 10, {});
+  test_all_interpret<zi>(F::LEQ, 10, {});
+  test_all_interpret<zi>(F::LT, 10, {});
+  // Dual
+  test_exact_interpret<zd>(F::NEQ, 10, {});
+  test_exact_interpret<zd>(F::EQ, 10, {});
+  test_all_interpret<zd>(F::GEQ, 10, {});
+  test_all_interpret<zd>(F::GT, 10, {});
+}
+
+// `a` and `b` are supposed ordered and `a <= b`.
 template <typename A>
 void join_meet_generic_test(A a, A b) {
   // Reflexivity
@@ -95,40 +114,78 @@ TEST(ZTest, JoinMeet) {
   join_meet_generic_test(zi(0), zi(1));
   join_meet_generic_test(zi(-10), zi(10));
   join_meet_generic_test(zi(Limits<int>::top() - 1), zi::top());
+  // Dual
+  join_meet_generic_test(zd::bot(), zd::top());
+  join_meet_generic_test(zd(1), zd(0));
+  join_meet_generic_test(zd(10), zd(-10));
+  join_meet_generic_test(zd(Limits<int>::bot() + 1), zd::top());
 }
 
 TEST(ZTest, Refine) {
   EXPECT_EQ(zi(0).refine(), false);
   EXPECT_EQ(zi::top().refine(), false);
   EXPECT_EQ(zi::bot().refine(), false);
+  // Dual
+  EXPECT_EQ(zd(0).refine(), false);
+  EXPECT_EQ(zd::top().refine(), false);
+  EXPECT_EQ(zd::bot().refine(), false);
+}
+
+template<typename A>
+void generic_entailment_test(A element) {
+  EXPECT_EQ(element.entailment(A::top()), false);
+  EXPECT_EQ(element.entailment(A::bot()), true);
+  EXPECT_EQ(A::bot().entailment(A::bot()), true);
+  EXPECT_EQ(A::top().entailment(A::top()), true);
+  EXPECT_EQ(A::top().entailment(A::bot()), true);
+  EXPECT_EQ(A::top().entailment(element), true);
 }
 
 TEST(ZTest, Entailment) {
   EXPECT_EQ(zi(0).entailment(zi(0)), true);
+  EXPECT_EQ(zi(1).entailment(zi(0)), true);
   EXPECT_EQ(zi(0).entailment(zi(1)), false);
-  EXPECT_EQ(zi(0).entailment(zi::top()), false);
-  EXPECT_EQ(zi(0).entailment(zi::bot()), true);
   EXPECT_EQ(zi(0).entailment(zi(-1)), true);
-  EXPECT_EQ(zi::bot().entailment(zi::bot()), true);
-  EXPECT_EQ(zi::top().entailment(zi::top()), true);
-  EXPECT_EQ(zi::top().entailment(zi::bot()), true);
-  EXPECT_EQ(zi::top().entailment(zi(0)), true);
+  EXPECT_EQ(zi(-1).entailment(zi(0)), false);
+  generic_entailment_test(zi(0));
+  // Dual
+  EXPECT_EQ(zd(0).entailment(zd(0)), true);
+  EXPECT_EQ(zd(1).entailment(zd(0)), false);
+  EXPECT_EQ(zd(0).entailment(zd(1)), true);
+  EXPECT_EQ(zd(0).entailment(zd(-1)), false);
+  EXPECT_EQ(zd(-1).entailment(zd(0)), true);
+  generic_entailment_test(zd(0));
 }
 
-typedef DArray<zi, StandardAllocator> SplitSeq;
+template<typename A>
+using SplitSeq = DArray<A, StandardAllocator>;
 
-SplitSeq make_singleton(zi x) {
-  return SplitSeq({x});
+template<typename A>
+SplitSeq<A> make_singleton(A x) {
+  return SplitSeq<A>({x});
 }
 
-SplitSeq make_empty() {
-  return SplitSeq();
+template<typename A>
+SplitSeq<A> make_empty() {
+  return SplitSeq<A>();
+}
+
+template<typename A>
+void generic_split_test(A element) {
+  EXPECT_EQ(element.split(), make_singleton(element));
+  EXPECT_EQ(A::top().split(), make_empty<A>());
+  EXPECT_EQ(A::bot().split(), make_singleton(A::bot()));
 }
 
 TEST(ZTest, Split) {
-  EXPECT_EQ(zi(0).split(), make_singleton(zi(0)));
-  EXPECT_EQ(zi::top().split(), make_empty());
-  EXPECT_EQ(zi::bot().split(), make_singleton(zi::bot()));
+  generic_split_test(zi(0));
+  generic_split_test(zd(0));
+}
+
+template<typename A>
+void generic_deinterpret_test() {
+  EXPECT_EQ(A::bot().deinterpret(), F::make_true());
+  EXPECT_EQ(A::top().deinterpret(), F::make_false());
 }
 
 TEST(ZTest, Deinterpret) {
@@ -140,6 +197,15 @@ TEST(ZTest, Deinterpret) {
   zi z9 = zi::bot().interpret(EXACT, f9).value();
   F f9_bis = z9.deinterpret();
   EXPECT_EQ(f10, f9_bis);
-  EXPECT_EQ(zi::bot().deinterpret(), F::make_true());
-  EXPECT_EQ(zi::top().deinterpret(), F::make_false());
+  generic_deinterpret_test<zi>();
+  // Dual
+  F f10_d = make_x_op_i(F::LEQ, 0, 10, standard_allocator);
+  zd z10_d = zd::bot().interpret(EXACT, f10_d).value();
+  F f10_bis_d = z10_d.deinterpret();
+  EXPECT_EQ(f10_d, f10_bis_d);
+  F f11_d = make_x_op_i(F::LT, 0, 11, standard_allocator);
+  zd z11_d = zd::bot().interpret(EXACT, f11_d).value();
+  F f11_bis_d = z11_d.deinterpret();
+  EXPECT_EQ(f10_d, f11_bis_d);
+  generic_deinterpret_test<zd>();
 }
