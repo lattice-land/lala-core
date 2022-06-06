20e923fb0ce4e342c56e90516e2619dc7393e634
Pierre Talbot <ptalbot@hyc.io>
2022-06-06 10:29:50 +0200

    lala-core is now header only library, because nvlink is not capable of dealing well with libraries (multiple definition error).

--- include/ast.hpp
+++ include/ast.hpp
@@ -103,7 +103,13 @@ using AVar = int;
 #define AID(v) (v & ((1 << 8) - 1))
 #define VID(v) (v >> 8)
 
-CUDA AVar make_var(AType type, int var_id);
+CUDA inline AVar make_var(AType atype, int var_id) {
+  assert(atype >= 0);
+  assert(var_id >= 0);
+  assert(atype < (1 << 8));
+  assert(var_id < (1 << 23));
+  return (var_id << 8) | atype;
+}
 
 /** The approximation of a formula in an abstract domain w.r.t. the concrete domain. */
 enum Approx {
@@ -151,7 +157,35 @@ enum Sig {
 
 namespace battery {
   template<>
-  CUDA void print(const lala::Sig& sig);
+  CUDA inline void print(const lala::Sig& sig) {
+    using namespace lala;
+    switch(sig) {
+      case NEG: printf("-"); break;
+      case ADD: printf("+"); break;
+      case SUB: printf("-"); break;
+      case MUL: printf("*"); break;
+      case DIV: printf("/"); break;
+      case MOD: printf("%%"); break;
+      case POW: printf("^"); break;
+      case EQ: printf("="); break;
+      case LEQ: printf("<="); break;
+      case GEQ: printf(">="); break;
+      case NEQ: printf("!="); break;
+      case GT: printf(">"); break;
+      case LT: printf("<"); break;
+      case AND: printf("/\\"); break;
+      case OR: printf("\\/"); break;
+      case IMPLY: printf("=>"); break;
+      case EQUIV: printf("<=>"); break;
+      case NOT: printf("!"); break;
+      case XOR: printf("xor"); break;
+      case JOIN: printf("|_|"); break;
+      case MEET: printf("|-|"); break;
+      default:
+        assert(false);
+        break;
+    }
+  }
 }
 
 namespace lala {
@@ -758,7 +792,7 @@ public:
 };
 
 /** Given a formula `f` and an environment, return the first abstract variable (`AVar`) occurring in `f` or `{}` if `f` has no variable in `env`. */
-template <typename F, typename Allocator>
+template <class F, class Allocator>
 CUDA thrust::optional<AVar> var_in(const F& f, const VarEnv<Allocator>& env) {
   const auto& g = var_in(f);
   switch(g.index()) {
