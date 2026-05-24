c1f094967db4ef7d0515c37d438b767a2ad1e51b
ytsao <tasocc456@gmail.com>
2026-05-24 12:45:32 +0200

    update is_extractable without using is_fsolution

--- include/lala/vstore.hpp
+++ include/lala/vstore.hpp
@@ -483,26 +483,14 @@ public:
    * If the strategy is `atoms`, we check the domains are singleton.
    */
   template<class ExtractionStrategy = NonAtomicExtraction>
-  CUDA bool is_extractable(const ExtractionStrategy& strategy = ExtractionStrategy(), const float epsilon = 1e-6) const {
+  CUDA bool is_extractable(const ExtractionStrategy& strategy = ExtractionStrategy()) const {
     if(is_bot()) {
       return false;
     }
-    using value_type = decltype(data[0].ub().value());
-    if constexpr(std::is_floating_point_v<value_type>) {
-      if constexpr(strategy.atoms) {
-        for(int i = 0; i < data.size(); ++i) {
-          if(battery::sub_up(data[i].ub().value(), data[i].lb().value()) > epsilon) {
-            return false;
-          }
-        }
-      }
-    }
-    else {
-      if constexpr(ExtractionStrategy::atoms) {
-        for(int i = 0; i < data.size(); ++i) {
-          if(data[i].lb().value() != data[i].ub().value()) {
-            return false;
-          }
+    if constexpr(ExtractionStrategy::atoms) {
+      for(int i = 0; i < data.size(); ++i) {
+        if(data[i].lb().value() != data[i].ub().value()) {
+          return false;
         }
       }
     }
@@ -511,7 +499,7 @@ public:
 
 #ifdef __CUDACC__
   template<class ExtractionStrategy = NonAtomicExtraction>
-  __device__ bool is_extractable(auto& group, const ExtractionStrategy& strategy = ExtractionStrategy(), const float epsilon = 1e-6) const {
+  __device__ bool is_extractable(auto& group, const ExtractionStrategy& strategy = ExtractionStrategy()) const {
     if(is_bot()) {
       return false;
     }
@@ -521,19 +509,9 @@ public:
         res = true;
       }
       group.sync();
-      using value_type = decltype(data[0].ub().value());
-      if constexpr(std::is_floating_point_v<value_type>) {
-        for(int i = group.thread_rank(); i < data.size(); i += group.num_threads()) {
-          if(battery::sub_down(data[i].ub().value(), data[i].lb().value()) > epsilon) {
-            res = false;
-          }
-        }
-      }
-      else {
-        for(int i = group.thread_rank(); i < data.size(); i += group.num_threads()) {
-          if(data[i].lb().value() != data[i].ub().value()) {
-            res = false;
-          }
+      for(int i = group.thread_rank(); i < data.size(); i += group.num_threads()) {
+        if(data[i].lb().value() != data[i].ub().value()) {
+          res = false;
         }
       }
       group.sync();
